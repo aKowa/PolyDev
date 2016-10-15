c172e4351255f5b4e49536372644a096b8492638
Andre Kowalewski <kowaandre@gmail.com>
2016-10-15 13:47:52 +0200

    [ADDED] RandomGeometry

---
+++ PolyDev/Assets/Random/Geometry/Scripts/ASpheroidGenerator.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+namespace PolyDev.Random.Geometry
+{
+	[RequireComponent( typeof (MeshFilter) )]
+	public abstract class ASpheroidGenerator : MonoBehaviour
+	{
+		private Mesh mesh;
+
+		protected Mesh Mesh
+		{
+			get
+			{
+				if ( mesh != null )
+				{
+					return mesh;
+				}
+				return mesh = this.GetComponent<MeshFilter>().sharedMesh;
+			}
+		}
+
+		private GameObject spheres;
+
+		protected GameObject Spheres
+		{
+			get
+			{
+				if ( spheres != null )
+				{
+					return spheres;
+				}
+				spheres = new GameObject( "Spheres" );
+				spheres.transform.parent = this.transform;
+				spheres.transform.localPosition = Vector3.zero;
+				return spheres;
+			}
+		}
+
+
+		public abstract void Generate();
+
+
+		public virtual void Start()
+		{
+			Generate();
+		}
+
+
+		// Debug Methods
+		protected virtual void Update()
+		{
+			//ShowNormals( Mesh );
+		}
+
+		protected void ShowNormals( Mesh normalMesh )
+		{
+			for ( int i = 0; i < normalMesh.vertexCount; i++ )
+			{
+				Debug.DrawRay( normalMesh.vertices[i], normalMesh.normals[i] * 0.1f, Color.red );
+			}
+		}
+
+		public void Reset()
+		{
+			var sphereGO = GameObject.CreatePrimitive( PrimitiveType.Sphere );
+			this.GetComponent<MeshFilter>().sharedMesh = sphereGO.GetComponent<MeshFilter>().sharedMesh;
+			DestroyImmediate( sphereGO );
+
+			if ( Spheres == null ) return;
+
+			DestroyImmediate( Spheres );
+			spheres = null;
+		}
+
+		protected Mesh CopyMesh( Mesh passedMesh )
+		{
+			return new Mesh()
+			{
+				vertices = passedMesh.vertices,
+				bindposes = passedMesh.bindposes,
+				normals = passedMesh.normals,
+				boneWeights = passedMesh.boneWeights,
+				bounds = passedMesh.bounds,
+				colors = passedMesh.colors,
+				colors32 = passedMesh.colors32,
+				hideFlags = passedMesh.hideFlags,
+				name = passedMesh.name,
+				subMeshCount = passedMesh.subMeshCount,
+				triangles = passedMesh.triangles
+			};
+		}
+	}
+}
