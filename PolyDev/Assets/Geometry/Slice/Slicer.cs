fa5eabf8ea1f07c1b9b4b05611f1cafd81360faf
Andre Kowalewski <kowaandre@gmail.com>
2016-10-16 15:33:45 +0200

    [INITILIZED] Slicer

---
+++ PolyDev/Assets/Geometry/Slice/Slicer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent( typeof( Collider ) )]
+public class Slicer : MonoBehaviour 
+{
+	private void Start () 
+	{
+		
+	}
+	
+	private void Update () 
+	{
+		
+	}
+}
