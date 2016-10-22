bdd1b01274a27f5a73dd9f546821dfb9e8c5b71b
Andre Kowalewski <kowaandre@gmail.com>
2016-10-22 14:15:39 +0200

    [COMMENTS] in Binder class

--- PolyDev/Assets/UI/Scripts/Binder.cs
+++ PolyDev/Assets/UI/Scripts/Binder.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 namespace PolyDev.UI
 {
+	/// <summary>
+	/// Binder class.
+	/// </summary>
+	/// <typeparam name="TValue">Value type.</typeparam>
+	/// <typeparam name="TTarget">Type of target Component.</typeparam>
 	public class Binder<TValue,TTarget> where TTarget : Component
 	{
-		private TValue value;
 		private readonly TTarget target;
 		private readonly PropertyInfo propInfo;
 
+		/// <summary>
+		/// Constructs Binder object.
+		/// </summary>
+		/// <param name="mono">Behavior that has the target Component attached. </param>
+		/// <param name="propertyName">The name of the property that should be bound to this value.</param>
 		public Binder ( MonoBehaviour mono, string propertyName )
 		{
 			target = mono.GetComponent<TTarget> ();
@@ -20,17 +29,30 @@ namespace PolyDev.UI
 			propInfo = target.GetType ().GetProperty ( propertyName );
 		}
 
-
-
-
+		/// <summary>
+		/// Value Field, which updates bound Component property in Setter.
+		/// </summary>
 		public TValue Value
 		{
-			get { return value; }
+			get { return ValueUnbound; }
 			set
 			{
-				this.value = value;
-				propInfo.SetValue( target, Convert.ChangeType( value, propInfo.PropertyType ), null );
+				this.ValueUnbound = value;
+				UpdateBoundProperty();
 			}
 		}
+
+		/// <summary>
+		/// Updates the bound property to this value.
+		/// </summary>
+		public void UpdateBoundProperty()
+		{
+			propInfo.SetValue ( target, Convert.ChangeType ( this.ValueUnbound, propInfo.PropertyType ), null );
+		}
+
+		/// <summary>
+		/// Gets and Sets value without updating the bound property. Note: Use UpdateBoundProperty() to do so.
+		/// </summary>
+		public TValue ValueUnbound { get; set; }
 	}
 }
